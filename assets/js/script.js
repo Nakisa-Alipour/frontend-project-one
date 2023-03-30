3a641c968985d4da404f9c55eacf5c6e8921fa30
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-03-30 18:16:06 +1100

    adds js code to display 10 youtube videos related to search query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,45 @@
-var btnWatchTrailer = $(".btn-watch");
-var video = "";
-var videos = $("#videos");
-$(document).ready(function () {
-  var API_KEY = "AIzaSyCEBhpZEX-qkQ_sqW5O_cR7MS9-Pa7kaC0";
+var btnWatchTrailerEl = document.querySelector(".btn-watch");
+var movieNameEl = document.querySelector("#movie-name");
+var videos = document.querySelector("#videos");
+var videoContainerEl = document.querySelector("#video-container");
+var API_KEY = "AIzaSyCEBhpZEX-qkQ_sqW5O_cR7MS9-Pa7kaC0";
 
-  btnWatchTrailer.on("click", function (event) {
-    event.preventDefault();
+function watchTrailer(event) {
+  event.preventDefault();
 
-    // getting the value or text the user enters in the search box
-    var searchQuery = $("#movie-name").val();
+  // getting the value or text the user enters in the search box
+  var searchQuery = movieNameEl.value.trim();
 
+  if (searchQuery) {
     searchVideo(API_KEY, searchQuery, 10);
-  });
+  }
+}
+function searchVideo(key, search, max_results) {
+  var apiUrl =
+    "https://www.googleapis.com/youtube/v3/search?key=" +
+    key +
+    "&type=video&part=snippet&maxResults=" +
+    max_results +
+    "&q=" +
+    search;
 
-  function searchVideo(key, search, max_results) {
-    videos.empty();
-    $.get(
-      "https://www.googleapis.com/youtube/v3/search?key=" +
-        key +
-        "&type=video&part=snippet&maxResults=" +
-        max_results +
-        "&q=" +
-        search,
-      function (data) {
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
         console.log(data);
 
-        data.items.forEach((item) => {
-          video = `
-            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+        for (var i = 0; i < data.items.length; i++) {
+          var videoEl = document.createElement("div");
+          videoEl.classList = "youtube-video";
+          videoEl.innerHTML += `
+            <iframe width="420" height="315" src="http://www.youtube.com/embed/${data.items[i].id.videoId}" frameborder="0" allowfullscreen></iframe>
             `;
-          videos.append(video);
-        });
-      }
-    );
-  }
-});
+          videos.append(videoEl);
+          videoContainerEl.append(videos);
+        }
+      });
+    }
+  });
+}
+
+btnWatchTrailerEl.addEventListener("click", watchTrailer);
